f754ad89cc0d99425698fbb2c09fd51f054ca07a
acacore <mensart81@outlook.com>
2025-09-04 12:18:13 +0000

    Create a generator to fetch and process data in batches from the users database

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -15,12 +15,20 @@ def connect_to_prodev():
 #connecting to the prodev database
 db_conn = connect_to_prodev()
 
-def stream_users():
+def stream_users_in_batches(batch_size):
     cur = db_conn.cursor()
-    cur.execute("SELECT * FROM user_data")
+    cur.execute(f"SELECT * FROM user_data WHERE age > 25 LIMIT {batch_size}")
 
     for row in cur:
         yield row
 
-# for user in stream_users():
-#     print(user)
+def batch_processing(batch_size=25):
+    for user in stream_users_in_batches(batch_size):
+        print(user)
+    
+
+if __name__ == "__main__":
+    batch_size=25
+    stream_users_in_batches(batch_size)
+    batch_processing(batch_size)
+    db_conn.close()
