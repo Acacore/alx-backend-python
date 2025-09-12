efe4c145ec30c2e5d79222a15a82e205e6fa9776
acacore <mensart81@outlook.com>
2025-09-12 13:56:14 +0000

    Adding a return statment for async_fetch_users(), async_fetch_older_users() and printing the result in forfetch_concurrently()

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -7,19 +7,20 @@ async def async_fetch_users():
     async with aiosqlite.connect("users.db") as conn:
         async with conn.execute("SELECT * FROM users") as cursor:
             rows = await cursor.fetchall()
-            for row in rows:
-                print(row)
+            return rows
 
 
 async def async_fetch_older_users():
     async with aiosqlite.connect("users.db") as conn:
         async with conn.execute("SELECT * FROM users WHERE age > 40") as cursor:
             rows = await cursor.fetchall()
-            for row in rows:
-                print(row)
+            return rows
     
 async def fetch_concurrently():
-    await asyncio.gather(async_fetch_users(), async_fetch_older_users())
+    rows = await asyncio.gather(async_fetch_users(), async_fetch_older_users())
+    
+    for row in rows:
+        print(row)
 
 if __name__ == "__main__":
     asyncio.run(fetch_concurrently())
