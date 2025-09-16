829021437ae1a40bf61fc3185db4e48afaee84e0
acacore <mensart81@outlook.com>
2025-09-16 23:51:30 +0000

    move README.md into the 0x03-Unittests_and_integration_tests dir

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python3
+
+"""This module provides utilities for accessing nested mappings safely."""
+
 import unittest
 from client import *
 from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
+    """MyClass stores and manipulates some data."""
     @parameterized.expand(
             [
                 ({"a": 1}, ("a",), 1, int),
@@ -11,6 +16,7 @@ class TestAccessNestedMap(unittest.TestCase):
              ]
     )
     def test_acccess_int_and_str(self, nested_map, path,  expected):
+        """Return the sum of two integers x and y."""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result,  expected)
 
