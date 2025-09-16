224555b7d341b27c1bcf4a9071dc9055863a443c
acacore <mensart81@outlook.com>
2025-09-16 23:24:45 +0000

    a TestAccessNestedMap class that inherits from unittest.TestCase with @parameterized.expand

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,22 @@
+import unittest
+from client import *
+from parameterized import parameterized
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand(
+            [
+                ({"a": 1}, ("a",), 1, int),
+                ({"a": {"b": 2}}, ("a",), {"b":2}, dict),
+                ({"a": {"b": 2}}, ("a", "b"), 2, int)   
+             ]
+    )
+    def test_acccess_int_and_str(self, nested_map, path,  expected, expeted_type):
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result,  expected)
+        self.assertIsInstance(result, expeted_type)
+
+
+
+if __name__== "__main__":
+    unittest.main()
+
