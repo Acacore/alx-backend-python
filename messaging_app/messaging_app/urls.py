7a05b3f8c941f41a33f4bfb77c587b59c2faafcb
acacore <mensart81@outlook.com>
2025-09-20 05:35:47 +0000

    sadded 'api/' to the main urls.py file

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -34,7 +34,7 @@ router.register(r'conversation', ConversationViewSet, 'conversation')
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # path("", include("chats.urls")),
+    path("api/", include("chats.urls")),
     path("", include(router.urls)),
     path('accounts/', include('allauth.urls')),
 ]
