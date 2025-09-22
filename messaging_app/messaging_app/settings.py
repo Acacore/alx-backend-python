320612563acea4def0a2fa4377100890940f8c57
acacore <mensart81@outlook.com>
2025-09-22 14:31:06 +0000

    permissions.py added

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -67,9 +67,9 @@ ROOT_URLCONF = "messaging_app.urls"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
