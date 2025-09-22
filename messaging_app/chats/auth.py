d64f3afd8c15b26ac716898b12c87fd92d3408a3
acacore <mensart81@outlook.com>
2025-09-22 14:40:08 +0000

    auth.py added

---
+++ messaging_app/chats/auth.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class EmailOrUsernameBackend(ModelBackend):
+    """
+    Custom authentication backend that lets users log in with either
+    their username OR their email address.
+    """
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        # Accept both username and email as login fields
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(email=username)
+            except User.DoesNotExist:
+                return None
+
+        if user.check_password(password):
+            return user
+        return None
