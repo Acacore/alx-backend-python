2935d180b378a433a2e57ff0c48614f4652ab07c
acacore <mensart81@outlook.com>
2025-09-23 08:11:42 +0000

    Pagination setting altered

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -76,7 +76,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.CustomPagination',
     'PAGE_SIZE': 20,
 }
 
