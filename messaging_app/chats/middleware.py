6ad2b4550bfc4a9c7e0b1738b3916c7012f84909
acacore <mensart81@outlook.com>
2025-09-24 04:25:09 +0000

    midleware.py added

---
+++ messaging_app/chats/middleware.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from .models import *
+from rest_framework import 
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s -%(message)s',
+    handlers=[
+        logging.FileHandler('request_logs.log')
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+class RequestLoggingMiddleware:
+
+    def __init__(self,  get_response):
+        self.get_response = get_response
+        
+
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else 'Anonymous'
+        logger.info(f"User: {user} - Path: {request.path}")
+        response = self.get_response(request):
+        return response
+
+    
