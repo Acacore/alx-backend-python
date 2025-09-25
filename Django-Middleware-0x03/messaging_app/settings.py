25ffd6d245f5a3decf971ec4143e11775f4eaeeb
acacore <mensart81@outlook.com>
2025-09-25 13:48:54 +0000

    chats.middleware.OffensiveLanguageMiddleware added to the settings.py file

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
     "chats.middleware.RequestLoggingMiddleware",
     "chats.middleware.RestrictAccessByTimeMiddleware",
+    "chats.middleware.OffensiveLanguageMiddleware",
     "chats.middleware.RolepermissionMiddleware"
 ]
 
