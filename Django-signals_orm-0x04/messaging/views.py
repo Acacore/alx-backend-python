3cd0b0de6cc31284658091bfbb2d359801a27760
acacore <mensart81@outlook.com>
2025-10-01 21:11:42 +0000

    def fetch_replies(message) and MessageViewset altered

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -19,10 +19,28 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessagingSerializer
     permission_classes = [IsAuthenticated]
 
-    class meta:
-        ...
+    def create(self, request):
+        sender = request.user
+        receiver = request.data.get('receiver')
+        Message.objects.filter(receiver=request.user)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(sender=sender, receiver=receiver)
 
 
+def fetch_replies(message):
+    replies = []
+    children = Message.objects.filter(parent_message=message)
+    for child in children:
+        replies.append({
+            "id": child.id,
+            "sender": child.sender.username,
+            "content": child.content,
+            "timestamp": child.timestamp,
+            "children": fetch_replies(child)  # recursion
+        })
+    return replies
+    
 def delete_user(request):
     if request.user.is_authenticated:
         user = get_object_or_404(User, pk=request.user.pk)
