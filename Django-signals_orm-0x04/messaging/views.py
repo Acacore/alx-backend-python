3f3f942374733d8baf48dd21814bf6a2bfc99b43
acacore <mensart81@outlook.com>
2025-10-01 21:20:52 +0000

    fetch_thread used insted

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -28,18 +28,34 @@ class MessageViewSet(viewsets.ModelViewSet):
         serializer.save(sender=sender, receiver=receiver)
 
 
-def fetch_replies(message):
-    replies = []
-    children = Message.objects.filter(parent_message=message)
-    for child in children:
-        replies.append({
-            "id": child.id,
-            "sender": child.sender.username,
-            "content": child.content,
-            "timestamp": child.timestamp,
-            "children": fetch_replies(child)  # recursion
-        })
-    return replies
+from collections import defaultdict
+
+def fetch_thread(root_message):
+    # 1. Pull all descendants in one query (or even all messages in the conversation)
+    all_messages = (
+        Message.objects
+        .filter(parent_message__isnull=False)   # all replies in DB
+        .select_related("sender", "receiver", "parent_message")  # avoids FK lookups
+    )
+
+    # 2. Group by parent_message_id (so we can build tree in memory)
+    children_map = defaultdict(list)
+    for msg in all_messages:
+        children_map[msg.parent_message_id].append(msg)
+
+    # 3. Recursive builder using the in-memory map
+    def build_tree(message):
+        return {
+            "id": message.id,
+            "sender": message.sender.username,
+            "receiver": message.receiver.username,
+            "content": message.content,
+            "timestamp": message.timestamp,
+            "children": [build_tree(child) for child in children_map[message.id]]
+        }
+
+    return build_tree(root_message)
+
     
 def delete_user(request):
     if request.user.is_authenticated:
