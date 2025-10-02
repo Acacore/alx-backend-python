889f31190b7fc177fcc2f69650f805e6f8442cb1
acacore <mensart81@outlook.com>
2025-10-02 08:47:50 +0000

    manager.py renamed into managers.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from manager import UnreadMessageManager
+from managers import UnreadMessageManager
 
 
 
