0f81a51f19d7dbc15e73e1b6da366186fe6346fe
acacore <mensart81@outlook.com>
2025-10-02 07:11:59 +0000

    user_message function added

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth import admin
 from django.contrib.auth.models import User
 from django.utils import timezone
 
@@ -14,11 +13,14 @@ class Message(models.Model):
     receiver = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="recieved_messages"
     )
-    parent_message = models.ForeignKey(self, null=True, blank=True, on_delete=models.CASCADE)
+    parent_message = models.ForeignKey('Message', null=True, blank=True, on_delete=models.CASCADE)
     content = models.TextField()
     edited = models.BooleanField(default=False)
     timestamp = models.DateTimeField(default=timezone.now)
 
+
+    def __str__(self):
+        return self.content[:30]
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
